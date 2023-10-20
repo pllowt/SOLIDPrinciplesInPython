8691da7c43e742e120e6de4862ccf81265f0c8d0
Paul Lowther <paul.lowther@ons.gov.uk>
2023-10-20 10:38:33 +0100

    Minor improvements

--- single_responsibility_bad.py
+++ single_responsibility_bad.py
@@ -9,6 +9,10 @@ class Report:
         self.title = title
         self.content = content
 
+    def write_report(self, file_path: str):
+        with open(file_path, 'w') as file:
+            file.write(f"{self.title}/n{self.content}")
+
     def edit_title(self, new_title: str):
         self.title = new_title
 
@@ -17,3 +21,4 @@ if __name__ == '__main__':
     new_report = Report(
         'Reporty Mcreportface', "this is some content inside of a report, I'm reporty mcreportface"
     )
+    new_report.write_report('./test_file.txt')
