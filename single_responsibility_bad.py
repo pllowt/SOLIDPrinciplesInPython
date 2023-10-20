bfc1971aad2ae24e3be45ad7229a945a68bf6542
Paul Lowther <paul.lowther@ons.gov.uk>
2023-10-20 11:18:11 +0100

    Minor improvements

--- single_responsibility_bad.py
+++ single_responsibility_bad.py
@@ -9,11 +9,16 @@ class Report:
         self.title = title
         self.content = content
 
+    def write_report(self, file_path: str):
+        with open(file_path, 'w') as file:
+            file.write(f"{self.title}\n{self.content}")
+
     def edit_title(self, new_title: str):
         self.title = new_title
 
 
 if __name__ == '__main__':
     new_report = Report(
-        'Reporty Mcreportface', "this is some content inside of a report, I'm reporty mcreportface"
+        'Reporty Mcreportface', "This is some content inside of a report, I'm reporty mcreportface"
     )
+    new_report.write_report('./bad_report.txt')
--- single_responsibility_good.py
+++ single_responsibility_good.py
@@ -17,10 +17,11 @@ class ReportWriter:
     @staticmethod
     def write_report(report: Report, file_path: str):
         with open(file_path, 'w') as file:
-            file.write(f"{report.title}/n{report.content}")
+            file.write(f"{report.title}\n{report.content}")
 
 
 if __name__ == '__main__':
     new_report = Report(
-            'Reporty Mcreportface', "this is some content inside of a report, I'm reporty mcreportface"
+            'Reporty Mcreportface', "This is some content inside of a report, I'm reporty mcreportface"
         )
+    ReportWriter.write_report(new_report, "./good_report.txt")
